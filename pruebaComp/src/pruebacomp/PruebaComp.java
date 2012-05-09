a7b547b2d03e794a1782a223a96a7b3ce76de4ac
s107e18 <s107e18@sala107_18>
2012-05-09 06:45:06 -0500

    Se insertó un mensaje de saludo general.

---
+++ pruebaComp/src/pruebacomp/PruebaComp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacomp;
+
+/**
+ *
+ * @author s107e18
+ */
+public class PruebaComp
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        System.out.println("Saludos");
+    }
+}
